d8555d5d5825ddee157f17b5bd4c30601b86f35e
DomAB2020 <51772450+DominicABrooks@users.noreply.github.com>
2022-12-20 17:55:18 -0600

    change function declaration

--- utils/generate-random.js
+++ utils/generate-random.js
@@ -2,7 +2,7 @@ module.exports = {
     getRndInteger: function (min, max) {
         return Math.floor(Math.random() * (max - min) ) + min;
     },
-    rollDice(number, sides) {
+    rollDice: function (number, sides) {
         const diceRolls = [];
         for (let i = 0; i < number; i++) {
           diceRolls.push(Math.floor(Math.random() * sides) + 1);
