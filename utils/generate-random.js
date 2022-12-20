3d1523465d33dfbe5eff35499aee6a2e0626e3ac
DomAB2020 <51772450+DominicABrooks@users.noreply.github.com>
2022-12-20 17:49:37 -0600

    add new random type

--- utils/generate-random.js
+++ utils/generate-random.js
@@ -2,4 +2,11 @@ module.exports = {
     getRndInteger: function (min, max) {
         return Math.floor(Math.random() * (max - min) ) + min;
     },
+    rollDice(number, sides) {
+        const diceRolls = [];
+        for (let i = 0; i < number; i++) {
+          diceRolls.push(Math.floor(Math.random() * sides) + 1);
+        }
+        return diceRolls;
+    }
 };
