91e1555060f3bf80fbc5334ca48e1c54bb770009
DomAB2020 <51772450+DominicABrooks@users.noreply.github.com>
2022-12-20 18:05:22 -0600

    fixed documentation

--- commands/roll.js
+++ commands/roll.js
@@ -15,10 +15,14 @@ module.exports = {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
         
-        const number = interaction.options.getInteger('number') ?? 1; // gets the number of dice to roll
-        const sides = interaction.options.getInteger('sides') ?? 6; // gets the number of sides on dice
-		const arr = genRandom.rollDice(number, sides); // calls rollDice fuction for accurate dice rolls, adds each roll to array arr
-		const sum = arr.reduce((acc, cur) => acc + cur, 0); // sums array arr of dice rolls for a total
+        // gets the number & sides of dice to roll
+        const number = interaction.options.getInteger('number') ?? 1; 
+        const sides = interaction.options.getInteger('sides') ?? 6; 
+
+        // calls rollDice fuction for accurate dice rolls, adds each roll to array arr
+		const arr = genRandom.rollDice(number, sides); 
+        // sums array arr of dice rolls for a total
+		const sum = arr.reduce((acc, cur) => acc + cur, 0); 
 
         await interaction.reply(`${sum}`);
     },
