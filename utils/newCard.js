6f0f3821f2eac2a57c8ccc9dcaabe5c4805c0463
DomAB2020 <51772450+DominicABrooks@users.noreply.github.com>
2022-12-24 11:42:42 -0600

    delete new card

--- utils/newCard.js
+++
@@ -1,8 +0,0 @@
-// initalizes a card
-module.exports = {
-    newCard: function(shade, type, number){
-        const color = shade;
-        const suit = type;
-        const value = number; 
-    }
-}
