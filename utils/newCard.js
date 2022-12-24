43ece282f5df8f45395e0f4a862a1593e02a73c4
Man-Of-The-Lake <raidorr16@gmail.com>
2022-12-24 12:40:11 -0500

    made newCard util and newDeck util

---
+++ utils/newCard.js
@@ -0,0 +1,8 @@
+// initalizes a card
+module.exports = {
+    newCard: function(shade, type, number){
+        const color = shade;
+        const suit = type;
+        const value = number; 
+    }
+}
